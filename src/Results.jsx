cf1add712ec77a1bc8a98e9fdeae9b3c5ca09c82
Elise <eliseraney@gmail.com>
2023-01-31 00:21:55 -0700

    Fixed an error in data retrieval

--- src/Results.jsx
+++ src/Results.jsx
@@ -6,7 +6,7 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2 className="headword">{props.results.foreach.hwi.hw}</h2>
+        <h2 className="headword">{props.results[0].hwi.hw}</h2>
         <h4 className="pronunciation">{props.results[0].hwi.prs[0].mw}</h4>
         <h5 className="functional-label">{props.results[0].fl}</h5>
         <p>{props.results[0].shortdef[0]}</p>
