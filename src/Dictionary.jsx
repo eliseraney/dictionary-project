33f95ef7ae4adb84be83fe226562b3bcc08c55a5
Elise <eliseraney@gmail.com>
2023-01-30 21:31:15 -0700

    API integration

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
+
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function handleSearch(event) {
     event.preventDefault();
-    alert(`Searching for definition of ${keyword}`);
+
+    const apiKey = `4b5921d9-1756-47f5-ad62-611d835b40f4`;
+    const apiUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${keyword}?key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
